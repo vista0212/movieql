db57a7e76ae641139fbc75b3c3955b3d1b786464
kb4ek <kbaek1202@gmail.com>
2020-01-31 03:10:11 +0900

    first commit

---
+++ graphql/db.js
@@ -0,0 +1,16 @@
+import fetch from 'node-fetch';
+
+const API_URL = "https://yts.am/api/v2/list_movies.json?";
+
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
+  }
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
+
+  console.log(REQUEST_URL);
+  return fetch(REQUEST_URL).then(res => res.json()).then(json => json.data.movies)
+}
---
+++ graphql/resolvers.js
@@ -0,0 +1,9 @@
+import { getMovies } from './db';
+
+const resolvers = {
+  Query: {
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
+  }
+};
+
+export default resolvers;
